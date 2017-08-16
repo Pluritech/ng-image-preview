1908775b9dd131ecfc207af85d8d0a782b46b76e
Lucas Correa <lucasccorrea@gmail.com>
2017-08-16 14:41:30 -0300

    inclusao do blur

--- src/image-preview/image-preview.component.ts
+++ src/image-preview/image-preview.component.ts
@@ -2,29 +2,34 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'image-preview',
-  template: `<img class="img-prev" [src]="sourceImage" [alt]="alt" (load)="loadImage()">`,
+  template: `<img class="img-prev" [src]="sourceImage" [alt]="alt" (load)="loadImage()" [ngClass]="{'img-blur': isBlur}">`,
   styleUrls: ['./image-preview.component.css']
 })
 export class ImagePreviewComponent implements OnInit {
 
+  public isBlur: boolean;
+
   @Input() urlFallback = './images/fallbackImage.jpg';
   @Input() urlLittle: string;
   @Input() urlBig: string;
-  @Input() alt: string;
   public sourceImage: string
   public typeImage: string
 
   constructor() {
+    console.log('Hello ImageInstagramComponent Component');
     this.typeImage = 'little';
   }
 
   public loadImage() {
     if (this.typeImage === 'little') {
+      this.isBlur = true;
       this.sourceImage = this.urlBig;
       this.typeImage = 'big';
     } else {
+      this.isBlur = false;
       return;
     }
+
   }
 
   ngOnInit() {
