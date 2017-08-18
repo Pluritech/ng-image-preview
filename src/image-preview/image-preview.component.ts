b3d19ab2ccff5f2540d679e47fb0e6cf62ac28ec
mduraes1994 <mduraes1994@gmail.com>
2017-08-18 14:25:18 -0300

    Update everything in project

--- src/image-preview/image-preview.component.ts
+++ src/image-preview/image-preview.component.ts
@@ -1,40 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'image-preview',
-  template: `<img class="img-prev" [src]="sourceImage" [alt]="alt" (load)="loadImage()" [ngClass]="{'img-blur': isBlur}">`,
+  templateUrl: './image-preview.component.html',
   styleUrls: ['./image-preview.component.css']
 })
 export class ImagePreviewComponent implements OnInit {
 
+  @Input() hqImage;
+  @Input() lowImage;
+  @Input() bgPlaceholder;
+  @Input() paddingBottom;
+  
+  @ViewChild('placeholder') placeholder: ElementRef;
+  @ViewChild('qualityImage') qualityImage: ElementRef;
+  constructor() {}
 
-  @Input() urlFallback = './images/fallbackImage.jpg';
-  @Input() urlLittle: string;
-  @Input() urlBig: string;
-  @Input() alt: string;
-
-  public sourceImage: string
-  public typeImage: string
-  public isBlur: boolean;
-
-  constructor() {
-    console.log('Hello ImageInstagramComponent Component');
-    this.typeImage = 'little';
+  private loadSmallImage(): void {
+    const imgSmall = this.placeholder.nativeElement.querySelector('.image-small');
+    imgSmall.src = this.lowImage;
+    imgSmall.onload = () => {
+      imgSmall.classList.add('loaded');
+    };
   }
 
-  public loadImage() {
-    if (this.typeImage === 'little') {
-      this.isBlur = true;
-      this.sourceImage = this.urlBig;
-      this.typeImage = 'big';
-    } else {
-      this.isBlur = false;
-      return;
-    }
-
+  private loadHqImage(): void {
+    const imgLarge = new Image();
+    imgLarge.src = this.hqImage;
+    imgLarge.classList.add('image-item');
+    
+    imgLarge.onload = () => {
+      this.qualityImage.nativeElement.classList.add('loaded');
+    };
   }
 
   ngOnInit() {
-    this.sourceImage = this.urlLittle;
+    this.loadSmallImage();
+    this.loadHqImage();
   }
 }
